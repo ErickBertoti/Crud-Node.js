049854bc71dd663584b7d58a074662cbce85f61e
ErickBertoti <erickhoffmann2301@gmail.com>
2024-10-28 20:45:40 -0300

    initial commit

---
+++ db/firebase.js
@@ -0,0 +1,18 @@
+const { initializeApp } = require('firebase/app')
+const { getFirestore } = require('firebase/firestore')
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCgdlS-TNv_b5FUDUj5_IBurD8go8bmgKY",
+  authDomain: "aula-firebase-b0aca.firebaseapp.com",
+  projectId: "aula-firebase-b0aca",
+  storageBucket: "aula-firebase-b0aca.appspot.com",
+  messagingSenderId: "884544847522",
+  appId: "1:884544847522:web:0a8865031314be89b6a1fb"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+module.exports = db
